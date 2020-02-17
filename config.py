1b1f7f11fb1846d684b30014d2a1fd7f6d43c3c4
Olivier71 <olivier.nesme@gmail.com>
2020-02-17 16:09:38 +0100

    Add MVC files and requirements + config

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# DATABASE
+DB_CONF = {
+    'host': 'localhost',
+    'user': 'off_user',
+    'password': 'XXX',
+    'db': 'off_base',
+	'raise_on_warnings': True
+}
---
+++ controller.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from models import Db
+
+database=Db()
