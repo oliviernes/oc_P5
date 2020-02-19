ef966d3bf3f0d6126fdd181f6c7b89400615a647
Olivier71 <olivier.nesme@gmail.com>
2020-02-19 11:14:29 +0100

    Create sql commands
    To generate tables in the database

---
+++ open_food_db.sql
@@ -0,0 +1,26 @@
+-------------------------------------------
+--SQL commands to create tables in the DB--
+-------------------------------------------
+
+CREATE TABLE category (
+                id INT AUTO_INCREMENT NOT NULL,
+                nom VARCHAR(150) NOT NULL,
+                PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+CREATE TABLE product (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(150) NOT NULL,
+                nutrition_grades VARCHAR(1) NOT NULL,
+                energy_100 DECIMAL(6,2) NOT NULL,
+                category_id INT NOT NULL,
+                PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+ALTER TABLE product ADD CONSTRAINT category_product_fk
+FOREIGN KEY (category_id)
+REFERENCES category (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
