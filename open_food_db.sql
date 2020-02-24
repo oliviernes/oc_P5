3724e1300f1ef7553e93dc13a791ae0bd8abd81b
Olivier71 <olivier.nesme@gmail.com>
2020-02-24 11:41:46 +0100

    Change column "name" in "category" table
    Change the physical data model and sql commands with sql power architect

--- open_food_db.sql
+++ open_food_db.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE category (
                 id INT AUTO_INCREMENT NOT NULL,
-                nom VARCHAR(150) NOT NULL,
+                name VARCHAR(150) NOT NULL,
                 PRIMARY KEY (id)
-)ENGINE=InnoDB;
+);
 CREATE TABLE product (
                 id INT AUTO_INCREMENT NOT NULL,
                 name VARCHAR(150) NOT NULL,
@@ -11,7 +11,8 @@ CREATE TABLE product (
                 energy_100 DECIMAL(6,2) NOT NULL,
                 category_id INT NOT NULL,
                 PRIMARY KEY (id)
-)ENGINE=InnoDB;
+);
+
 
 ALTER TABLE product ADD CONSTRAINT category_product_fk
 FOREIGN KEY (category_id)
