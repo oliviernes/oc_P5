0c2e980b8d6a3e25e9fbe1fcfa3290fa04966e7e
Olivier71 <olivier.nesme@gmail.com>
2020-02-25 10:01:55 +0100

    Format with black and add files to .gitignore

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-
 # DATABASE
 DB_CONF = {
-    'host': 'localhost',
-    'user': 'off_user',
-    'password': 'XXX',
-    'db': 'off_base',
-	'raise_on_warnings': True
+    "host": "localhost",
+    "user": "off_user",
+    "password": "XXX",
+    "db": "off_base",
+    "raise_on_warnings": True,
 }
--- models.py
+++ models.py
@@ -1,32 +1,34 @@
 from mysql.connector import errorcode
 import requests
-#~ import pdb
+
+# ~ import pdb
 from config import DB_CONF
 
 ################
 #   DATABASE   #
 ################
 
-class Db():
-	"""Manage the database"""
-	
-	def __init__(self):
-	
-		con_conf = {
-			'host': DB_CONF['host'],
-			'user': DB_CONF['user'],
-			'password': DB_CONF['password'],
-		}
 
-		try:
-			cnx = mysql.connector.connect(**con_conf)
+class Db:
+    """Manage the database"""
 
-		except mysql.connector.Error as err:
-			if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-				print("Something is wrong with your user name or password")
-			elif err.errno == errorcode.ER_BAD_DB_ERROR:
-				print("Database does not exist")
-			else:
-				print(err)
-		else:
-			cnx.close()
+    def __init__(self):
+
+        con_conf = {
+            "host": DB_CONF["host"],
+            "user": DB_CONF["user"],
+            "password": DB_CONF["password"],
+        }
+
+        try:
+            cnx = mysql.connector.connect(**con_conf)
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            cnx.close()
