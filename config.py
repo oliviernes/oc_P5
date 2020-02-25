4c3ab91b4aee20d26b3fd63a79985e86ab73782e
Olivier71 <olivier.nesme@gmail.com>
2020-02-25 18:13:50 +0100

    Add autocommit parameter and categories dict
    Add autocommit to make changes persistent in the database.

--- config.py
+++ config.py
@@ -4,5 +4,14 @@ DB_CONF = {
     "user": "off_user",
     "password": "XXX",
     "db": "off_base",
+	"autocommit": True,
     "file": "open_food_db.sql",
+}
+
+CATEGORIES = {
+	"Biscuits",
+	"Yaourts",
+	"Pâtisseries",
+	"Glaces",
+	"Tartes",
 }
