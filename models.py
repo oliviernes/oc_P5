155f1c8d8fb4a541668ee1bd9afa10afe6138004
Olivier71 <olivier.nesme@gmail.com>
2020-02-26 18:59:37 +0100

    clean input_data and add drop_table method

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import mysql.connector
@@ -6,7 +6,7 @@ from mysql.connector import errorcode
 import requests
 import json
 # ~ import pdb
-from config import DB_CONF
+from config import DB_CONF, API_URL
 
 ################
 #   DATABASE   #
@@ -94,62 +94,65 @@ class Db:
                 print("OK")
         
         #~ cursor.close()
-    
 
-    def input_data(self, db):
+    def drop_table(self, table):
+        """ DROP TABLE Products"""
+
+        connection = self.cnx
+        cursor = connection.cursor()
+        query = f"DROP TABLE IF EXISTS `{table}`;"
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+
+
+###########################
+#       Category          #
+###########################
+
+class Category:
+    """ Class generating a category"""
+    
+    def __init__(self, cat_name):
+        self.name = cat_name
+
+    def parameters(self, page):
 
         search_param = {
-            "search_terms": ", biscuits,",
+            "search_terms": "Tartes",
             "search_tag": "categories_tag",
+            "sort_by": "unique_scans_n",
             "page_size": 10,
             "json": 1,
-            "page": 1,
+            "page": page,
         }
-
-        API_URL = "https://fr-en.openfoodfacts.org/cgi/search.pl?"
+        
+        return search_param
+        
+    def input_data(self, db):    
+    
         products = []
         # initialize to page 1 of search result
         page = 1
-        req = requests.get(API_URL, params=search_param)
+        req = requests.get(API_URL, params=self.parameters(page))
 
         # output of request as a json file
         req_output = req.json()
         # list of product of the output
         products_output = req_output["products"]
-        # store product classes
-        # ~ for product in products_output:
-        # ~ products.append(Product_API(product))
-
-        prod0 = products_output[0]
-
-        prod_reduit = {}
-        cat_stage = {}
-
-        prod_reduit["name"] = prod0["product_name_fr"]
-        prod_reduit["nutrition_grades"] = prod0["nutrition_grades"]
-        prod_reduit["energy_100"] = prod0["nutriments"]["energy_100g"]
-
-        cat_stage["name"] = "biscuits"
-
-        prod_names = []
-
-        for prod in products_output:
-            prod_names.append(prod["product_name_fr"])
-
-        #########################
 
         sql_list = []
-        insert_cat = """INSERT INTO category (`name`) VALUES ('{}');"""
-        insert_prod = """INSERT INTO product (`name`, `nutrition_grades`, `energy_100`, `category_id`) \
-        SELECT "{name}", "{nutrition_grades}", "{energy_100}", id AS category_id \
-        FROM category \
-        WHERE name = "{cat}";"""
+        insert_cat = "INSERT INTO category (`name`) VALUES ('{}');"
+        insert_prod = """INSERT INTO product (`name`, \
+        `nutrition_grades`, `energy_100`, `category_id`) \
+        SELECT "{name}", "{nutrition_grades}", "{energy_100}", \
+        id AS category_id FROM category WHERE name = "{cat}";"""
 
         # insert category
         sql_list.append(insert_cat.format("biscuits",))
 
         # insert products
-        for idx, val in enumerate(req_output["products"]):
+        for val in req_output["products"]:
             sql_list.append(
                 insert_prod.format(
                     name=val["product_name_fr"],
@@ -159,12 +162,8 @@ class Db:
                 )
             )
 
-        # ~ db = models.Db()
-
         connection = db.cnx
         cursor = connection.cursor()
-
-        # ~ print(sql_list)
 
         try:
             for command in sql_list:
