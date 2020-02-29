466b4174feeab174933ffee266aff630c1660ce0
Olivier71 <olivier.nesme@gmail.com>
2020-02-29 16:58:52 +0100

    Add a pytest file

---
+++ test_oc-P5.py
@@ -0,0 +1,21 @@
+import pytest
+from config import DB_CONF
+from models import Db
+
+### Connexion to the db:
+
+def test_connexion1():
+    DB_CONF["user"] = 'bob'
+    
+    with pytest.raises(AssertionError):
+        assert Db.__init__(Db)=="Something is wrong with your user name\
+         or password"
+
+def test_connexion2():
+    DB_CONF['user'] = 'off_user'
+    DB_CONF['db'] = 'foobar'
+    
+    with pytest.raises(AssertionError):
+        assert Db.__init__(Db)=="1044 (42000): Access denied for user \
+        'off_user'@'localhost' to database 'foobar'"
+
