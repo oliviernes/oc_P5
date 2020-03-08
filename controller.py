d6e5e6269f45f5625f08e7ba58cc595b98ede095
Olivier71 <olivier.nesme@gmail.com>
2020-03-08 10:10:30 +0100

    Add os.system('clear')
    To clear the screen between inputs

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import view
 from models import Db
 
@@ -11,19 +12,21 @@ def cli():
 
     view.greeting()
 
-    breakpoint()
-
     records_cat = database.get_infos_category()
 
     view.display_categories(records_cat)
 
     choice = input("\nChoose the index of one of the categories: ")
 
+    os.system('clear')
+		
     records_prod = database.get_infos_product(choice)
 
     view.display_category_product(choice, records_cat, records_prod)
 
     choice = input("\nChoose the index of one of the products: ")
+
+    os.system('clear')
 
     view.display_products(choice, records_prod)
 
--- view.py
+++ view.py
@@ -3,6 +3,7 @@
 from models import Db
 from colorama import Fore, Style
 
+
 def greeting():
     """ function to greet app's users"""
     print(
@@ -52,30 +53,33 @@ def display_products(pick, records_prod):
                 selected.append(row[0])
         print(
             "\nNo healthier product available. However, you can \
-choose a product with more or less energy:\n"
+choose a product with more or less energy. The list is sorted by \
+ energy values in kj per 100g or 100mL. The red values are greater\
+ than the energy of the product picked or green if equal\
+ or less:\n"
         )
     else:
         print(
             "\nHere is a list of healthier products (sorted by their\
  energy values in kj per 100g or 100mL). The red values are greater\
- than the energy in kj of the product picked or green if equal\
+ than the energy of the product picked or green if equal\
  or less:\n"
         )
 
-    selected_energy=records_prod[index][3]
-    
+    selected_energy = records_prod[index][3]
+
     sorted_by_energy = sorted(records_prod, key=lambda tup: tup[3])
 
     for idx, val in enumerate(sorted_by_energy):
         if val[0] in selected:
             for i in range(4):
-                if i<3:
+                if i < 3:
                     print(val[i], " ", end="")
                 else:
-                    if val[3]>selected_energy:
-                        print(Fore.RED+str(val[3]), " ", end="")
+                    if val[3] > selected_energy:
+                        print(Fore.RED + str(val[3]), " ", end="")
                         print(Style.RESET_ALL)
                     else:
-                        print(Fore.GREEN+str(val[3]), " ", end="")
+                        print(Fore.GREEN + str(val[3]), " ", end="")
                         print(Style.RESET_ALL)
             print("\n")
