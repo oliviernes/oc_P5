d20e8453dd9cd25f66c165db48a2ecce94f0c1d6
Olivier71 <olivier.nesme@gmail.com>
2020-03-10 16:49:27 +0100

    Remove column substitute_id in records_pro
    To clean the product's list because of all(prod) which return false if substitute_id is none

--- view.py
+++ view.py
@@ -26,7 +26,11 @@ def display_category_product(pick, records_cat, records_prod):
     print("\nYou chose the category", records_cat[pick - 1][1], ":\n")
 
     """Cleaning of the product's list:"""
-    records_prod=[prod for prod in records_prod if all(prod)]
+    records_prod_dirty=[]
+    for row in records_prod:
+        records_prod_dirty.append(row[:4])
+        
+    records_prod=[prod for prod in records_prod_dirty if all(prod)]
        
     for row in records_prod:
         print(row[0], ":", row[1], "(", row[2], ")")
