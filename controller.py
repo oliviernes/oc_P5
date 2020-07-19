f5f3a42b229b7a6bd0c875cff712fceaa5f09195
Olivier71 <olivier.nesme@gmail.com>
2020-07-19 21:47:18 +0200

    Create class Command and class Control in controller.py

--- controller.py
+++ controller.py
@@ -7,142 +7,148 @@ from view import Display
 
 database = Db()
 
+class Control:
 
-def substitution(substitutes):
+    def __init__(self):
 
-    selection = [(1, 1), (2, 2)]
+        self.selection = [(1, 1), (2, 2)]
 
-    if len(substitutes[0]) == 0:
-        print("There is no substitutes recorded")
-    elif len(substitutes[0]) > 0:
-        answer = check_input(
-            "\nSome substitutes are available. Do you \
+    def substitution(self, substitutes):
+
+        if len(substitutes[0]) == 0:
+            print("There is no substitutes recorded")
+        elif len(substitutes[0]) > 0:
+            answer = self.check_input(
+                "\nSome substitutes are available. Do you \
 want to visit them?\n\n1:no\n2:yes\n\nYour answer: ",
-            selection,
-        )
-        if answer == 2:
-            print("\nHere are your substitutes: ")
-            for subs in substitutes[0]:
-                for prods in substitutes[1]:
-                    if subs[0] == prods[0]:
-                        prod_name = prods[1]
-                    elif subs[1] == prods[0]:
-                        subs_name = prods[1]
-                print("\n", prod_name, " can be replaced by ", subs_name)
-    print("\nHere are the categories available for screening:\n")
+                self.selection,
+            )
+            if answer == 2:
+                print("\nHere are your substitutes: ")
+                for subs in substitutes[0]:
+                    for prods in substitutes[1]:
+                        if subs[0] == prods[0]:
+                            prod_name = prods[1]
+                        elif subs[1] == prods[0]:
+                            subs_name = prods[1]
+                    print("\n", prod_name, " can be replaced by ", subs_name)
+        print("\nHere are the categories available for screening:\n")
 
 
-def check_input(question, records):
+    def check_input(self, question, records):
 
-    select = []
+        select = []
 
-    for elements in records:
-        select.append(elements[0])
+        for elements in records:
+            select.append(elements[0])
 
-    num = input(question)
+        num = input(question)
 
-    while True:
-        try:
-            val = int(num)
-            if val > 0 and val in select:
-                break
-            elif val > 0 and val not in select:
-                num = input(
-                    "Input must be one of the index displayed.\
- Try again: "
-                )
-            else:
-                num = input(
-                    "Input must be a positive integer.\
- Try again: "
-                )
-        except ValueError:
+        while True:
             try:
-                float(num)
-                num = input(
-                    "Input is a float number. You must enter an \
-interger number. Please, try again: "
-                )
+                val = int(num)
+                if val > 0 and val in select:
+                    break
+                elif val > 0 and val not in select:
+                    num = input(
+                        "Input must be one of the index displayed.\
+ Try again: "
+                    )
+                else:
+                    num = input(
+                        "Input must be a positive integer.\
+ Try again: "
+                    )
             except ValueError:
-                num = input(
-                    "This is not a number.\
+                try:
+                    float(num)
+                    num = input(
+                        "Input is a float number. You must enter an \
+interger number. Please, try again: "
+                    )
+                except ValueError:
+                    num = input(
+                        "This is not a number.\
  Please enter a valid number. Try again: "
-                )
+                    )
 
-    return val
+        return val
 
+class Command:
 
-def cli():
+    def cli(self):
 
-    display = Display()
+        display = Display()
 
-    display.greeting()
+        display.greeting()
 
-    continu = True
+        continu = True
+        
+        control = Control()
+        
+        while continu:
 
-    while continu:
+            database = Db()
 
-        database = Db()
+            substitutes = database.get_substitute()
 
-        substitutes = database.get_substitute()
+            control.substitution(substitutes)
 
-        substitution(substitutes)
+            records_cat = database.get_infos_category()
 
-        records_cat = database.get_infos_category()
+            display.display_categories(records_cat)
 
-        display.display_categories(records_cat)
+            choice = control.check_input(
+                "\nChoose the index of one of the categories:",
+                records_cat,
+            )
 
-        choice = check_input(
-            "\nChoose the index of one of the categories:\
- ",
-            records_cat,
-        )
+            os.system("clear")
 
-        os.system("clear")
+            records_prod = database.get_infos_product(choice)
 
-        records_prod = database.get_infos_product(choice)
+            records_prod = display.display_category_product(
+                choice, records_cat, records_prod
+            )
 
-        records_prod = display.display_category_product(
-            choice, records_cat, records_prod
-        )
+            choice_prod = control.check_input(
+                "\nChoose the index of one of the products:",
+                records_prod,
+            )
 
-        choice_prod = check_input(
-            "\nChoose the index of one of the products: \
-",
-            records_prod,
-        )
+            os.system("clear")
 
-        os.system("clear")
+            records_prod = display.display_products(choice_prod, records_prod)
 
-        records_prod = display.display_products(choice_prod, records_prod)
-
-        choice_subs = check_input(
-            "\nChoose the index of a product to \
+            choice_subs = control.check_input(
+                "\nChoose the index of a product to \
 substitute: ",
-            records_prod,
-        )
+                records_prod,
+            )
 
-        selec = [(1, 1), (2, 2)]
-        choice = check_input(
-            "\nDo you want to register this substitute in the\
+            selec = [(1, 1), (2, 2)]
+            choice = control.check_input(
+                "\nDo you want to register this substitute in the\
  database?\n\n1:no\n2:yes\n\nYour answer: ",
-            selec,
-        )
+                selec,
+            )
 
-        if choice == 2:
-            database.update_data(choice_prod, choice_subs)
-        else:
-            pass
+            if choice == 2:
+                database.update_data(choice_prod, choice_subs)
+            else:
+                pass
 
-        choice = check_input(
-            "\nDo you want to search other products?\n\n1:no\n2:yes\n\nYour answer: ",
-            selec,
-        )
+            choice = control.check_input(
+                "\nDo you want to search other products?\n\n1:no\n2:yes\n\n\
+Your answer: ",
+                selec,
+            )
 
-        if choice == 1:
-            break
-        else:
-            pass
+            if choice == 1:
+                break
+            else:
+                pass
 
+command = Command()
 
-cli()
+command.cli()
